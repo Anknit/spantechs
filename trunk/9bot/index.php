072f899a3d34a3d5989df9d112a63a02302a5215
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-02 08:55:03 +0000

    ninebot description, keywords, added in index and require both

--- trunk/9bot/index.php
+++ trunk/9bot/index.php
@@ -8,24 +8,21 @@
        	<link rel="stylesheet" type="text/css" href="../Common/css/pageTransition.css" />
 	   	<link rel="stylesheet" type="text/css" href="../Common/css/animations.css" />
 		<link rel="stylesheet" type="text/css" href="../Common/css/sequenceLetters.css" />
+        <meta name="description" content="ninebot è la soluzione intelligente per la mobilità del futuro. Elettrico e non inquinante, leggero e smontabile, trasportabile in macchina e nel gavone di una barca. Ninebot One Dal giovane manager al rapper, per girare in allegria e godere dell'ambiente che ci circonda, saltando con rapide evoluzioni il traffico e le barriere di città ormai troppo congestionate. Nini Robotics è la soluzione accessibile per tutti, anche per chi si trovava sino a ieri bloccato ed impedito da barriere architettoniche: scale o ascensori troppo stretti" />
+        <meta name="keywords" content="9botitalia,9bot italia,ninebot italy,ninebot italia,ninebot">
+        <meta name="author" content="ninebot italy">
 		<?php require_once 'require.php';?>
 		<link rel="stylesheet" type="text/css" href="css/Home_textShow.css" />
 		<script type="text/javascript" src="../Common/js/JsFunctionsToLocatePopUpdiv.js"></script>
 	</head>
 	<body>
-	    <span style="display:none;">ninebot è la soluzione intelligente per la mobilità del futuro. Elettrico e non inquinante, leggero e smontabile, trasportabile in macchina e nel gavone di una barca.
- 
-Ninebot One Dal giovane manager al rapper, per girare in allegria e godere dell'ambiente che ci circonda, saltando con rapide evoluzioni il traffico e le barriere di città ormai troppo congestionate.
- 
-Nini Robotics è la soluzione accessibile per tutti, anche per chi si trovava sino a ieri bloccato ed impedito da barriere architettoniche: scale o ascensori troppo stretti</span>	
 		<div id="pt-main" class="pt-perspective">
 			<div id="textSlideShow" class="pt-page" style="display:none;">
                 <div class="os-phrases" id="os-phrases">
-                    <h2>9botitalia.com</h2>
-                    <h2>official partner</h2>
-                    <h2>of ninebot.com</h2>
-                    <h2>Distributore Ufficiale per</h2>
-                    <h2>l'Italia dei Prodotti Ninebot</h2>
+                    <h2>New Moving NUOVO</h2>
+                    <h2>Partner Ninebot Inc.</h2>
+                    <h2>Distributore ninebot</h2>
+                    <h2>e nino robotics</h2>
                 </div>
                 <script>
                     $(document).ready(function() {
