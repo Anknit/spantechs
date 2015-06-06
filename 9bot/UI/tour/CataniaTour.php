9a1ae8ddf6d33c927bff5bb36b4be10924a1800e
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-06 03:58:40 +0000

    center aligned coming soon class

--- 9bot/UI/tour/CataniaTour.php
+++ 9bot/UI/tour/CataniaTour.php
@@ -5,7 +5,7 @@
         var pageIdentifier  =   'Tour';
     </script>
 	<div class="content">
-		<div class='workProgressContainer'>
+		<div class='workProgressContainer' style="margin-bottom:0">
 			<img src='../../images/icons/coming_soon.jpg' alt='progress' />
 		</div>
 	</div>
