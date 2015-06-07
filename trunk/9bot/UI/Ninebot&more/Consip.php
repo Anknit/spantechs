d93a638ce2e1d7840213a9ce279b43a6c04a6178
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-07 10:23:22 +0000

    consip altered

--- trunk/9bot/UI/Ninebot&more/Consip.php
+++ trunk/9bot/UI/Ninebot&more/Consip.php
@@ -11,20 +11,31 @@
 ?>
     <title>Consip</title>
     <link type="text/css" rel="stylesheet" href='../../css/Consip.css' />
+    <script src="../../../Common/js/jquery-ui.min.js" type="text/javascript"></script>
+    <script>
+        $(function(){
+            $('.tabs').tabs({
+              event: "mouseover"
+            });
+        });
+    </script>
     <script src="../../js/Consip.js" type="text/javascript"></script>
 	<div class="content" style="height:80%">
         <div class="pageHeading">
-            Le amministrazioni pubbliche potranno trovare i prodotti Ninebot all’interno del catalogo elettronico CONSIP nella sezione “Veicoli             elettrici”, sottosezione “Biciclette elettriche a pedalata assistita”.
+            Le amministrazioni pubbliche potranno trovare i prodotti Ninebot all’interno del catalogo elettronico CONSIP nella sezione “Veicoli elettrici”, sottosezione “Biciclette elettriche a pedalata assistita”.
         </div>
-    	<div class="GalleyForLinkToProduct">
-        </div>
-    	<div class="productDisplay">
-        	<div class="catalogPanel">
-                <div class="productName"></div>
-                <div class="productDescription"></div>
-                <div class="productPrice"></div>
+        <div class="tabs">
+            <ul>
+                <li><a href="#Ninebot_E">Ninebot E</a></li>
+                <li><a href="#Ninebot_Police">Ninebot Police</a></li>
+                <li><a href="#Miscellaneous">Miscellaneous</a></li>
+            </ul>
+            <div id="Ninebot_E" style="float:left !important;">
             </div>
-            <div class="catalogRightPanel"></div>    
+            <div id="Ninebot_Police">
+            </div>
+            <div id="Miscellaneous">
+            </div>
         </div>
 	</div>
 <?php
