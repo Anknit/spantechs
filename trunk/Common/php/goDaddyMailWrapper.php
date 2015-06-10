53c348edf392c9c98d2ed07dbe90cde464007044
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-10 13:18:57 +0000

    mail body, make payment

--- trunk/Common/php/goDaddyMailWrapper.php
+++ trunk/Common/php/goDaddyMailWrapper.php
@@ -10,7 +10,30 @@
 $smtpSettings	=	array(
 	'sender'	=>	'ninebot@9botitalia.com'
 );
+	
+function get_Mail_Content($MailBody	=	"")
+{
+	if($MailBody == "")
+		$output	=	false;
+	else
+	$output	=	'<html>
+				<head>
+					<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+					<title>9bot italia</title></head>
+						<style type="text/css"> .internalTextNormal{
+						font-family: Arial, Helvetica, sans-serif;
+						font-size: 11px;
+						color: #000000;
+						font-weight: normal;	 
+					}</style>
+				</head>
+				<body>'.$MailBody.'</body></html>';
+				
+	return $output;	
+}
+
 function send_Email($recipients, $mailSubject, $MailBody){
+	$MailBody	=	get_Mail_Content($MailBody);
 	global $smtpSettings;
 	$sender = "From: ".$smtpSettings['sender']."\r\n" .	"Reply-To: webmaster@" . $_SERVER['SERVER_NAME'] . "\r\n" .	"X-Mailer: PHP/" . phpversion();
 	mail($recipients, $mailSubject, $MailBody, $sender);
