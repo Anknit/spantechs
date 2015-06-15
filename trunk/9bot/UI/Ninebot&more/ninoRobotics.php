57dc60bf93547dcaa3376eb07e8a987ba4d13116
Anknit <Anknit@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-15 18:51:54 +0000

    nino-robotics completed

--- trunk/9bot/UI/Ninebot&more/ninoRobotics.php
+++ trunk/9bot/UI/Ninebot&more/ninoRobotics.php
@@ -10,9 +10,94 @@
 	require_once './header.php';
 ?>
     <title>Nino robotics</title>
+    <link type="text/css" rel="stylesheet" href='../../css/ninoLayout.css' />
 	<div class="content">
-		<div class="workProgressContainer" style="margin-bottom:0">
-			<img src='../../images/icons/coming_soon.jpg' alt="progress" />
+		<div class='topBanner'>
+			<img src='./../../images/nino/Animation%20image/1.png' alt='nino-robotics'>
+		</div>
+		<div class='infoDescNino'>
+			<h3>
+				Nino ha una collaborazione esclusiva con Ninebot Inc. per i motori. Tutto il resto è stato progettato, costruito e assemblato in Francia nel Lit et Mixe. 
+			</h3>
+			<p>
+				Nino ha volontariamente un design minimalista. Compatto, leggero, colorato e confortevole. Nino evidenzia il suo utente.
+				<br/>		
+				Progetto innovativo
+				<br/>		
+				In anticipo sui tempi: telaio in alluminio leggero, seduta regolabile e traspirante, manubrio rimovibile per un miglior comfort.
+				<br/>		
+				Una tecnologia di stabilizzazione brevettata.
+			</p>
+		</div>
+		<div class='imgLister'>
+		<!--
+			<div class='actionContainer left'>
+				<img src='./../../images/icons/left.png' alt='nino-robotics' />
+			</div>
+			<div class='actionContainer right'>
+				<img src='./../../images/icons/right.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO black 34.png' alt='nino-robotics' />
+			</div>
+		-->
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO black dessus.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO black dos.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO white 34.png' alt='nino-robotics' />
+			</div>
+		<!--
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO white dos.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/NINO white face.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/rendu black and white proto 2 34 tt 2.94.png' alt='nino-robotics' />
+			</div>
+			<div class='thumbContainer'>
+				<img src='./../../images/nino/Other%20nino%20png/rendu black and white proto 2 34 tt 2.96.png' alt='nino-robotics' />
+			</div>
+		-->
+		</div>
+		<div class='keyDetailNino'>
+			<ul>
+				<li>
+					Dimensioni: 59 x 64 x 50 cm
+				</li>
+				<li>
+					Larghezza sedile: 420 millimetri
+				</li>
+				<li>
+					Profondità sedile: 390 millimetri
+				</li>
+				<li>
+					Potenza: 2750 W
+				</li>
+				<li>
+					Peso macchina: 39 kg
+				</li>
+				<li>
+					Peso massimo consentito: 100 kg
+				</li>
+				<li>
+					Autonomia massima: 38 km
+				</li>
+				<li>
+					Tempo di ricarica: 3 ore
+				</li>
+				<li>
+					Pendenza massima: 20%
+				</li>
+				<li>
+					Velocità: tra 6 e 9 km/h, secondo le norme vigenti
+				</li>
+			</ul>
 		</div>
 	</div>
 <?php
