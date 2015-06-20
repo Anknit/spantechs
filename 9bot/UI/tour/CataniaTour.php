5d3d6433e1697ab524264424d6e41f6d7d733e04
Anknit <Anknit@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-20 09:26:41 +0000

    catania tour

--- 9bot/UI/tour/CataniaTour.php
+++ 9bot/UI/tour/CataniaTour.php
@@ -1,42 +1,186 @@
 <?php 
 	require_once './header.php';
 ?>
-    <script>
-        var pageIdentifier  =   'Tour';
-    </script>
-    <link href="./../../css/cataniaTour.css" rel="stylesheet" type="text/css"> 
-	<div class="content">
-    	<div class="cataniaTopHeader">
-        	<div class='cataniaTourThumbContainer'>
-            	<div class='cataniaTourThumb'>
-                	<a href='#basicCatania'>
-            	    	<h2>Segway Basic Tour</h2>
-                    </a>
-                </div>
-            	<div class='cataniaTourThumb'>
-                	<a href='#grandCatania'>
-        	        	<h2>Segway Grand Tour</h2>
-    	               </a>
-                </div>
-            	<div class='cataniaTourThumb'>
-                	<a href='#suggestiveCatania'>
-	                	<h2>Segway Suggestive Tour</h2>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class='cataniaTourContent'>
-        	<div class="cataniaContainer" id="basicCatania">
-               	<img src='./../../images/tours/catania/basic.png' alt='Segway Basic Tour'>
-            </div>
-        	<div class="cataniaContainer" id="grandCatania">
-               	<img src='./../../images/tours/catania/grand.png' alt='Segway Grand Tour'>
-            </div>
-        	<div class="cataniaContainer" id="suggestiveCatania">
-               	<img src='./../../images/tours/catania/suggestive.png' alt='Segway Suggestive Tour'>
-            </div>
-        </div>
+<script type="text/javascript">
+	var pageIdentifier  =   'Tour';
+</script>
+<link href="./../../css/cataniaTour.css" rel="stylesheet" type="text/css">
+<div class="content">
+	<div class='cataniaTourContent'>
+		<div class="cataniaContainer" id="basicCatania">
+			<div class=row1>
+				<div class='descInit'>
+					<h3>
+						<u>Segway Basic</u>
+					</h3>
+					<p>
+						Scegli un modo diverso di vivere la città sulle due ruote del
+						futuro; scopri la città con il divertente <span class='catTourName'>SEGWAY BASIC TOUR</span>, potrai
+						muoverti comodamente ed ammirare i luoghi più significativi della città:
+					</p>
+				</div>
+			</div>
+			<div class='row2'>
+				<div class='imgCat'>
+					<img src='./../../images/tours/catania/basic.png' alt='Segway Basic Tour'>
+				</div>
+				<div class='descFeat'>
+					<ul>
+						<li>Il Teatro Massimo, uno dei più grandi teatri dell'opera d'Europa;</li>
+						<li>La Cattedrale e la Piazza Duomo, con la statua dell’Elefante che la sovrasta;</li>
+						<li>Il mercato del pesce rievocante l'atmosfera di un souk arabo.</li>
+					</ul>
+				</div>
+			</div>
+			<div class='row3'>
+				<div class='descMore'>
+					<p>Vivi con noi la città con le due ruote più ecologiche ed avveniristiche del mondo!</p>
+				</div>
+				<div class='comp'>
+					<div class='comprande'>
+						<h3>La quota comprende:</h3>
+						<ul>
+							<li>Noleggio accompagnato del segway per 40 minuti;</li>
+							<li>Briefing iniziale fondamentale per la preparazione alla guida del segway;</li>
+							<li>Assicurazione;</li>
+							<li>Accompagnatore e istruttore segway.</li>
+						</ul>
+					</div>
+					<div class='nonComprande'>
+						<h3>La quota non comprende:</h3>
+						<ul>
+							<li>Cibo e bevande ove non espressamente specificato;</li>
+							<li>Costi d'ingresso ai siti archeologici e musei;</li>
+							<li>Eventuali mance e spese personali;</li>
+							<li>Guida turistica.</li>
+						</ul>
+					</div>
+				</div>
+				<div class='prezzo'>
+					<h3>PREZZO: 29€ pax</h3>
+				</div>
+			</div>
+		</div>
+		<div class="cataniaContainer" id="grandCatania">
+			<div class=row1>
+					<div class='descInit'>
+						<h3>
+							<u>Segway Grand</u>
+						</h3>
+						<p>
+							Scegli un modo diverso di vivere la città su avveniristiche due
+							ruote: scopri la città con il divertente <span class='catTourName'>SEGWAY GRAND TOUR</span>, potrai
+							ammirare i luoghi più significativi della città:
+						</p>
+					</div>
+			</div>
+			<div class='row2'>
+				<div class='imgCat'>
+					<img src='./../../images/tours/catania/grand.png' alt='Segway Grand Tour'>
+				</div>
+				<div class='descFeat'>
+					<ul>
+						<li>Il Teatro Massimo, uno dei più grandi teatri dell'opera d'Europa.</li>
+						<li>La Cattedrale e la Piazza Duomo, con la statua dell’Elefante che la sovrasta.</li>
+						<li>La via Etnea; salotto della città.</li>
+						<li>Le molteplici ed incantevoli chiese di Via dei Crociferi.</li>
+						<li>Il Castello Ursino, unica struttura dell'antica città medievale a essere sopravvissuta al terremoto ed all'eruzione del 1669.</li>
+					</ul>
+				</div>
+			</div>
+			<div class='row3'>
+				<div class='descMore'>
+					<p>
+						Vivi con noi la città con le due ruote più ecologiche ed avveniristiche del mondo!
+					</p>
+				</div>
+				<div class='comp'>
+					<div class='comprande'>
+						<h3>La quota comprende:</h3>
+						<ul>
+							<li>Noleggio accompagnato del segway per 1 ora e 30 minuti;</li>
+							<li>Briefing iniziale fondamentale per la preparazione alla guida del segway;</li>
+							<li>Assicurazione;</li>
+							<li>Istruttore e accompagnatore segway;</li>
+							<li>Coffee Break e Arancino.</li>
+						</ul>
+					</div>
+					<div class='nonComprande'>
+						<h3>La quota non comprende:</h3>
+						<ul>
+							<li>Cibo e bevande ove non espressamente specificato;</li>
+							<li>Costi d'ingresso ai siti archeologici e musei;</li>
+							<li>Eventuali mance e spese personali;</li>
+							<li>Guida turistica.</li>
+						</ul>
+					</div>
+				</div>
+				<div class='prezzo'>
+					<h3>PREZZO: 69€ pax</h3>
+				</div>
+			</div>
+		</div>
+		<div class="cataniaContainer" id="suggestiveCatania">
+			<div class=row1>
+				<div class='descInit'>
+					<h3>
+						<u>Segway Suggestive</u>
+					</h3>
+					<p>
+						Scegli un modo diverso di vivere la città su avveniristiche due
+						ruote, esplora comodamente la città, raggiungi i luoghi più
+						significativi con il <span class='catTourName'>SEGWAY SUGGESTIVE TOUR</span> un esperienza
+						indimenticabile per ammirare:
+					</p>
+				</div>
+			</div>
+			<div class='row2'>
+				<div class='imgCat'>
+					<img src='./../../images/tours/catania/suggestive.png' alt='Segway Suggestive Tour'>
+				</div>
+				<div class='descFeat'>
+					<ul>
+						<li>L'architettura medievale di Catania;</li>
+						<li>Lasciarti coinvolgere dalla vivace cultura siciliana;</li>
+						<li>Lasciarti incantare dagli splendidi esempi dell'architettura barocca siciliana;</li>
+						<li>La famosa Fontana dell'Elefante simbolo della città;</li>
+						<li>Il giardino Bellini, polmone verde della città e significativo esempio di giardino liberty.</li>
+					</ul>
+				</div>
+			</div>
+			<div class='row3'>
+				<div class='descMore'>
+					<p>
+						Vivi con noi la città con le due ruote più ecologiche ed avveniristiche del mondo!
+					</p>
+				</div>
+				<div class='comp'>
+					<div class='comprande'>
+						<h3>La quota comprende:</h3>
+						<ul>
+							<li>Noleggio segway accompagnato per 1 ora;</li>
+							<li>Briefing iniziale fondamentale per la preparazione alla guida del segway;</li>
+							<li>Assicurazione;</li>
+							<li>Istruttore ed accompagnatore segway.</li>
+						</ul>
+					</div>
+					<div class='nonComprande'>
+						<h3>La quota non comprende:</h3>
+						<ul>
+							<li>Cibo e bevande ove non espressamente specificato;</li>
+							<li>Costi d'ingresso ai siti archeologici e musei;</li>
+							<li>Eventuali mance e spese personali;</li>
+							<li>Guida turistica.</li>
+						</ul>
+					</div>
+				</div>
+				<div class='prezzo'>
+					<h3>PREZZO: 49€ pax</h3>
+				</div>
+			</div>
+		</div>
 	</div>
+</div>
 <?php 
 	require_once './footer.php';
 ?>
