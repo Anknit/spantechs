b2306f988203c463d87efff52887400095aa14e0
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-22 10:38:40 +0000

    send mail added to common

---
+++ trunk/aapastech/functionalities/appMailSending.php
@@ -0,0 +1,39 @@
+<?php
+/*
+* Author: Aditya
+* date: 08-Aug-2014
+* CSS Document 
+*
+*/
+?>
+<?php
+if(isset($_GET)){
+	$output	=	0;
+	if(isset($_GET['sendMail'])){
+		function includeWrapperAndSendMail($recipients, $mailSubject, $MailBody){
+			require_once '../Common/php/sharedHostingMailWrapper.php';
+			return send_Email($recipients, $mailSubject, $MailBody);
+		}
+	
+		if($_GET['sendMail'] == 'queryForm'){
+			$recipients	=	'aditya@aapastech.com, guptaaditya24@gmail.com';
+			$mailSubject=	'Urgent action required. Query from aapastech.com';
+			$MailBody=	'<div style="margin-left:5%">';
+				extract($_GET);
+				extract($_POST);
+				$MailBody	=	'<span>Senders Name:</span> '.$senderName;
+				$MailBody	=	'<span>Senders Email address:</span> '.$senderEmail;
+				$MailBody	=	'<span>Query Subject:</span> '.$querySubject;
+				$MailBody	=	'<span>Query Description:</span> '.$queryDescription;
+			$MailBody=	'</div>';
+			
+			$mailSent	=	includeWrapperAndSendMail($recipients, $mailSubject, $MailBody);
+			if($mailSent){
+				$output	=	1;
+			}
+			
+		}//send mail for queryForm
+	}//Send mail action
+	echo $output;
+}
+?>
