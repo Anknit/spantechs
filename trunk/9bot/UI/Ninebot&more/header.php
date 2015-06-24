99cac71d83bd63e4a9e5b2ef8504569396c7dd74
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-24 03:58:59 +0000

    golfcar tour jssor slider images, aapastech covering continents

--- trunk/9bot/UI/Ninebot&more/header.php
+++ trunk/9bot/UI/Ninebot&more/header.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="author" content="Aditya Gupta @ http://www.aapastech.com">
     <link rel="icon" href="../../images/favicon.ico" type="image/ico" >
     <script src="../../../Common/js/jquery.min.js" type="text/javascript"></script>
     <script src="../../../Common/js/setSelectionEffects.js" type="text/javascript"></script>
