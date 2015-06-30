f6a072a269f64759469f1e644ad4df3372c9d34c
Anknit <Anknit@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-30 16:37:17 +0000

    gallery added

---
+++ trunk/9bot/js/gallery.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('.galleryHead').on('click',function(){
+		$('.activeHead').removeClass('activeHead');
+		$(this).addClass('activeHead');
+		$('.activeContainer').removeClass('activeContainer');
+		$('#'+$(this).attr('data-show')).addClass('activeContainer');
+	});
+	$('#imagesContainer img').on('click',function(){
+		$('.modalWindow').css('display','block');
+		$('.imgConBody').html('');
+		$('.imgConBody').append('<img src="'+$(this).attr('src')+'" />');
+	});
+});
+var showPrevImage	=	function(){
+	var curSrc		=	$('.imgConBody img').attr('src');
+	var thumbCur	=	$('#imagesContainer img[src="'+curSrc+'"]').eq(0);
+	var newCont		=	thumbCur.parent().prev('div');
+	if(newCont.length == 0){return false;}
+	var newSrc		=	newCont.find('img').attr('src');
+	$('.imgConBody img').attr('src',newSrc);
+};
+var showNextImage	=	function(){
+	var curSrc		=	$('.imgConBody img').attr('src');
+	var thumbCur	=	$('#imagesContainer img[src="'+curSrc+'"]').eq(0);
+	var newCont		=	thumbCur.parent().next('div');
+	if(newCont.length == $('#imagesContainer img').length){return false;}
+	var newSrc		=	newCont.find('img').attr('src');
+	$('.imgConBody img').attr('src',newSrc);
+};
