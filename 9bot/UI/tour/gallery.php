692779cf2ce83ecf0bde077233967601acb3fff9
Anknit <Anknit@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-06-30 16:37:17 +0000

    gallery added

--- 9bot/UI/tour/gallery.php
+++ 9bot/UI/tour/gallery.php
@@ -2,64 +2,157 @@
 	require_once 'header.php';
 ?>
 <link href="../../css/gallery.css" rel="stylesheet" type="text/css" />
+<link href="../../css/style.css" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content="initial-scale=1, minimum-scale=1, target-densityDpi=device-dpi">
     <script>
         var pageIdentifier  =   'Gallery';
     </script>
 	<div class="content">
 		<div class="galleryContentContainer">
-			<div class="typeContainer" id="imagesContainer">
-				<table>
-					<tr>
-						<td><!--
-							<h2 class='galleryTourType'>TORRE ARGENTINA</h2>
-							--><div class="galleryImagesCardContainer" align="center" tour='TORRE_ARGENTINA'>
-								<img src ='../../images/tours/1.png' alt= 'TORRE_ARGENTINA' class='front card' />
-								<img src ='../../images/tours/2.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/3.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/4.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/5.png' alt= 'TORRE_ARGENTINA' class='back card' />
-							</div>
-						</td><!--
-						<td>
-							<h2 class='galleryTourType'>PINCIO - VILLA BORGHESE</h2>
-							<div class="galleryImagesCardContainer" align="center" tour='PINCIO_VILLA_BORGHESE'>
-								<img src ='../../images/tours/1.png' alt= 'PINCIO_VILLA_BORGHESE' class='front card' />
-								<img src ='../../images/tours/2.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/3.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/4.png' alt= 'PINCIO_VILLA_BORGHESE' class='middle card' />
-								<img src ='../../images/tours/5.png' alt= 'PINCIO_VILLA_BORGHESE' class='back card' />
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<h2 class='galleryTourType'>TRASTEVERE</h2>
-							<div class="galleryImagesCardContainer" align="center" tour='TRASTEVERE'>
-								<img src ='../../images/tours/1.png' alt= 'TRASTEVERE' class='front card' />
-								<img src ='../../images/tours/2.png' alt= 'TRASTEVERE' class='middle card' />
-								<img src ='../../images/tours/3.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/4.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/5.png' alt= 'TRASTEVERE' class='back card' />
-							</div>
-						</td>
-						<td>
-							<h2 class='galleryTourType'>CIRCUS MAXIMUS</h2>
-							<div class="galleryImagesCardContainer" align="center" tour='CIRCUS_MAXIMUS'>
-								<img src ='../../images/tours/1.png' alt= 'CIRCUS_MAXIMUS' class='front card' />
-								<img src ='../../images/tours/2.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/3.png' alt= 'TORRE_ARGENTINA' class='middle card' />
-								<img src ='../../images/tours/4.png' alt= 'CIRCUS_MAXIMUS' class='middle card' />
-								<img src ='../../images/tours/5.png' alt= 'CIRCUS_MAXIMUS' class='back card' />
-							</div>
-						</td>
-					--></tr>
-				</table>
+			<div class='headContain'>
+				<div class='galleryHead activeHead' data-show='imagesContainer'>
+					<h2>Photos</h2>
+				</div>
+				<div class='galleryHead' data-show='videosContainer'>
+					<h2>Videos</h2>
+				</div>
+			</div>
+			<div style='display:table'>
+			<div class="typeContainer activeContainer" id="imagesContainer">
+				<div>
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/2.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/3.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/4.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/5.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/6.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/7.jpg'>
+					</div><!--
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/3.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/4.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/5.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/6.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+					<div>
+						<img src='./../../images/tours/1.jpg'>
+					</div>
+				--></div>
 			</div>
 			<div class="typeContainer" id="videosContainer">
-				
+				<div>
+					<div>
+						<object data='http://www.youtube.com/embed/I_y2LOy-JLE'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/b5LvxOA_fY4'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/zAXRrCiphrA'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/8izbWCUvg9g'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/YTCc9mJyvpM'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/ztgebRfeYwQ'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/GTiYMPjqXzg'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/uzAqaccO47s'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/FtgZMB9SN1o'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/1hgD7Urkvp0'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/eIodswrTq0g'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/kDUJ9OX9Y5g'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/7_5tR8YdiLE'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/dKvdnTo_qIY'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/6cGsR4aoGNE'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/3g8wEPq0s2s'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/aAZAzHMzClc'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/Vv7QMfiIwWs'></object>
+					</div>
+					<div>
+						<object data='http://www.youtube.com/embed/_zn8SmrOMZ8'></object>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+	<div class='modalWindow'>
+		<div id='imgModalContainer'>
+			<div class='imgConHead'>
+				<div class='prevBut' onclick="showPrevImage();">
+					Prev
+				</div>
+				<div class='nextBut' onclick="showNextImage();">
+					Next
+				</div>
+				<div class='close' onclick="$('.modalWindow').css('display','none');">
+					Close
+				</div>
+			</div>
+			<div class='imgConBody'>
+			</div>
+			<div class='imgConFoot'>
+			</div>
+		</div>
+	</div>
+<script type="text/javascript" src='./../../js/gallery.js'></script>
 <?php 
 	require_once 'footer.php';
 ?>
