32857175bee631764b00f5d3854c05677a516dbb
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-07-02 19:00:27 +0000

    no more directory requirements

--- 9bot/Db.php
+++ 9bot/Db.php
@@ -12,8 +12,8 @@ function get_DbConfig(){
 	$config = array (							
 				'host'	=>	'localhost',
 				'port'	=>	'3306',
-				'username'	=>	'root',
-				'password'	=>	'',
+				'username'	=>	'rootNineBot',
+				'password'	=>	'calisto9',
 				'database'	=>	'booking_tours'
 			);
 	return $config;
