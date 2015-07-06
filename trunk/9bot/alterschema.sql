72fb4456d56b65945637e5fe84c497183d670d21
guptaaditya24 <guptaaditya24@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-07-06 03:48:05 +0000

    added a query to add a field to reseller info

--- trunk/9bot/alterschema.sql
+++ trunk/9bot/alterschema.sql
@@ -1,1 +1,3 @@
 alter table `booking_tours`.`newslogging` change `NewsTitle` `NewsTitle` text  CHARACTER SET utf8   NOT NULL , change `NewsDetails` `NewsDetails` text  CHARACTER SET utf8   NOT NULL 
+
+alter table `booking_tours`.`resellersinfo` add column `PriorityOrder` varchar (10)  NULL  after `ContactType`
