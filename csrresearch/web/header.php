fe7ab303d52b52e874e0bd72a5bafd5fe534b605
Anknit <Anknit@4fe2ea74-9c7c-4855-80d1-1fe3a29fe8d3>
2015-08-05 18:01:06 +0000

    sedfs

---
+++ csrresearch/web/header.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CSR-Research</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" href="css/demo.css" type="text/css" media="all">
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css/jquery.dropSlideMenu.css" type="text/css" media="all">
+</head>
+
+<body class="body">
+	<div class="wrapper">
+		<div class="wrapper_bg">
+			<div class="logoWrap">
+			    <div class="logoContainerHead">
+			    	<a href="index.html">
+			    		<img src="images/companylogo.png"/>
+			    	</a>
+			    </div>
+			</div>
+			<div id="menu_bg">
+				<div id="taglineHead">
+					<p><span>CSR</span> is a continued commitment of the business to social, environmental and economic development
+					</p>
+				</div>
+				<div id="navigation">
+					<ul>
+						<li><a href="./">HOME</a></li>
+						<li><a href="./?objectives">OBJECTIVES</a></li>
+			            <li><a href="./?pillars">PILLARS OF CSR</a>
+							<ul>
+								<li><a href="./?economicalpillar">Econmical Pillar</a></li>
+								<li><a href="./?socialpillar">Social Pillar</a></li>
+								<li><a href="./?environmentalpillar">Environmental Pillar</a></li>
+							</ul>
+						</li>
+						<li><a href="./?governing">GOVERNING BODY</a></li>
+						<li><a href="./?gallery">GALLERY</a></li>
+						<li><a href="./?sponsorship">SPONSORSHIP</a></li>
+						<li><a href="./?resourcepersons">RESOURCE PERSONS</a></li>					
+						<li><a href="./?contactus">CONTACT</a></li>
+					</ul>
+				</div>
+			</div>
+			<!--end of menu here //--->
+		</div>
+	</div>
